307e362bdcd443a14f6eed2c7c32e9679cb3d982
Yunong_LI <li@hnl.t.u-tokyo.ac.jp>
2023-11-14 21:20:01 +0900

    [Mini Quadrotor][Apriltag] Generation of a bundle of tags
    A new python code to generate a bundle of tags for landing.

---
+++ robots/mini_quadrotor/scripts/Picture_generate.py
@@ -0,0 +1,39 @@
+from PIL import Image
+
+# 打开6张图片
+image0 = Image.open("/home/lyn/ros/jsk_aerial_robot_ws/src/jsk_aerial_robot/aerial_robot_simulation/gazebo_model/models/Apriltag36_11_00000/materials/textures/tag36_11_00000.png")
+image1 = Image.open("/home/lyn/ros/jsk_aerial_robot_ws/src/jsk_aerial_robot/aerial_robot_simulation/gazebo_model/models/Apriltag36_11_00001/materials/textures/tag36_11_00001.png")
+image2 = Image.open("/home/lyn/ros/jsk_aerial_robot_ws/src/jsk_aerial_robot/aerial_robot_simulation/gazebo_model/models/Apriltag36_11_00002/materials/textures/tag36_11_00002.png")
+image3 = Image.open("/home/lyn/ros/jsk_aerial_robot_ws/src/jsk_aerial_robot/aerial_robot_simulation/gazebo_model/models/Apriltag36_11_00003/materials/textures/tag36_11_00003.png")
+image4 = Image.open("/home/lyn/ros/jsk_aerial_robot_ws/src/jsk_aerial_robot/aerial_robot_simulation/gazebo_model/models/Apriltag36_11_00004/materials/textures/tag36_11_00004.png")
+image5 = Image.open("/home/lyn/ros/jsk_aerial_robot_ws/src/jsk_aerial_robot/aerial_robot_simulation/gazebo_model/models/Apriltag36_11_00005/materials/textures/tag36_11_00005.png")
+
+
+# 设定固定大小
+new_width, new_height = 475, 475
+
+# 调整图片大小
+image0_resized = image0.resize((new_width, new_height))
+image1_resized = image1.resize((new_width, new_height))
+image2_resized = image2.resize((new_width, new_height))
+image3_resized = image3.resize((new_width, new_height))
+image4_resized = image4.resize((new_width, new_height))
+image5_resized = image5.resize((200, 200))
+
+
+# 创建一个新的图像
+result_width = 2100
+result_height = 2970
+result_image = Image.new("RGB", (result_width, result_height),(255,255,255,0))
+
+# 将图片粘贴到新图像上（你可以根据需要修改坐标）
+result_image.paste(image0_resized, (812, 1247))
+result_image.paste(image1_resized, (512, 627))
+result_image.paste(image2_resized, (1112, 627))
+result_image.paste(image3_resized, (512, 1867))
+result_image.paste(image4_resized, (1112, 1867))
+result_image.paste(image5_resized, (1512, 1384))
+
+
+# 保存结果
+result_image.save("result_image.jpg")
