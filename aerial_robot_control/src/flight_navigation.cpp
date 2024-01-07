1901a27619bb3f3e729fb8e5866daefbf07f2887
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2024-01-07 10:42:48 +0900

    [Mini_Quadrotor][config]Change the Landing parameter 1. Use secondstep.py file to achieve success. 2. changed the parameter of velocity and land_position_converge_fresh when land on in /aerial_robot_control/src/flight_navigation.cpp Congradulation! Never give up, ok?

--- aerial_robot_control/src/flight_navigation.cpp
+++ aerial_robot_control/src/flight_navigation.cpp
@@ -1039,14 +1039,14 @@ void BaseNavigator::rosParamInit()
   ros::NodeHandle nh(nh_, "navigation");
   getParam<int>(nh, "xy_control_mode", xy_control_mode_, 0);
   getParam<double>(nh, "takeoff_height", takeoff_height_, 0.0);
-  getParam<double>(nh, "land_descend_vel",land_descend_vel_, -0.3);
+  getParam<double>(nh, "land_descend_vel",land_descend_vel_, -0.5);
   getParam<double>(nh, "hover_convergent_duration", hover_convergent_duration_, 1.0);
   getParam<double>(nh, "land_check_duration", land_check_duration_, 1.0);
   getParam<double>(nh, "trajectory_reset_duration", trajectory_reset_duration_, 0.5);
   getParam<double>(nh, "teleop_reset_duration", teleop_reset_duration_, 0.5);
   getParam<double>(nh, "z_convergent_thresh", z_convergent_thresh_, 0.05);
   getParam<double>(nh, "xy_convergent_thresh", xy_convergent_thresh_, 0.15);
-  getParam<double>(nh, "land_pos_convergent_thresh", land_pos_convergent_thresh_, 0.02);
+  getParam<double>(nh, "land_pos_convergent_thresh", land_pos_convergent_thresh_, 0.01);
   getParam<double>(nh, "land_vel_convergent_thresh", land_vel_convergent_thresh_, 0.01);
 
   //*** trajectory
